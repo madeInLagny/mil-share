b95beab8888940271344b8a43e6d9fd5c22918ec
madeInLagny <madeInLagny@gmail.com>
2019-10-08 15:48:43 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# mil-share
-Webcomponent to share current url on social networks
+# \<mil-share\>
+
+'mil-share' is webcomponent that let share the current url on social networks and by email.
+Available links: Facebook, Twitter, Whatsapp, email.
+
+Demo: https://stackblitz.com/edit/mil-share-example?file=index.js
+
+## Usage
+
+### Install from npm
+
+```sh
+npm install --save mil-share
+```
+
+### Import to your webcomponent
+
+```js
+import "mil-share";
+```
+
+### Add to html
+
+```js
+<mil-share></mil-share>
+```
+
+## Styling
+
+Use customs CSS properties for styling.
+
+| CSS property           | Definition          | Default |
+| ---------------------- | ------------------- | ------- |
+| `--body-text-color`    | header font color   | #555    |
+| `--social-icon-width`  | social icons width  | 32px    |
+| `--social-icon-height` | social icons height | 32px    |
+| `--share-icon-color`   | share icon color    | #555    |
+| `--share-icon-height`  | share icon height   | 24px    |
+| `--share-icon-width`   | share icon width    | 24px    |
---
+++ mil-share.js
@@ -0,0 +1,160 @@
+import { LitElement, html, css } from "lit-element";
+import { fbLogo, twLogo, mailLogo, wappLogo, share } from "./social-icons.js";
+import "@polymer/iron-dropdown";
+
+class socialShare extends LitElement {
+  /**
+   * Declared properties and their corresponding attributes
+   */
+  static get properties() {
+    return {
+      fburl: { type: String },
+      twurl: { type: String },
+      emailurl: { type: String },
+      wappurl: { type: String },
+      header: { type: String }
+    };
+  }
+
+  /**
+   * Instance of the element is created/upgraded. Useful for initializing
+   * state, set up event listeners, create shadow dom.
+   * @constructor
+   */
+  constructor() {
+    super();
+    var url = encodeURIComponent(window.location.href);
+    this.fburl = "https://facebook.com/sharer/sharer.php?u=" + url;
+    this.twurl = "https://twitter.com/intent/tweet/?url=" + url;
+    this.emailurl = "mailto:?body=" + url;
+    this.wappurl = "https://api.whatsapp.com/send?phone=&text=" + url;
+    this.header = "Share this page";
+  }
+
+  static get styles() {
+    return [
+      css`
+        :host {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        #container {
+          display: inline-block;
+        }
+
+        a {
+          color: transparent;
+          padding-right: 10px;
+        }
+
+        a:first-of-type {
+          padding-left: 10px;
+        }
+
+        #dropdown {
+          min-width: 10rem;
+          padding: 0.5rem 0;
+          margin: 0.125rem 0 0;
+          font-size: 1rem;
+          color: var(--body-text-color, #555);
+          text-align: left;
+          list-style: none;
+          background-color: #ffffff;
+          background-clip: padding-box;
+          border: 1px solid rgba(0, 0, 0, 0.15);
+          border-radius: 0.25rem;
+          z-index: 100;
+          transform: translate3d(0, 0, 1px);
+          box-shadow: -2px 0 8px rgba(0, 0, 0, 0.3);
+        }
+
+        svg {
+          width: var(--social-icon-width, 32px);
+          height: var(--social-icon-height, 32px);
+        }
+
+        #dropdownIcon svg {
+          fill: var(--share-icon-color, #555);
+          height: var(--share-icon-height, 24px);
+          width: var(--share-icon-width, 24px);
+        }
+        #header {
+          padding: 10px;
+          font-size: 16px;
+          color: var(--body-text-color, #555);
+        }
+      `
+    ];
+  }
+
+  render() {
+    return html`
+      <div id="container">
+        <span id="dropdownIcon" @click="${this.openDropdown}">${share}</span>
+        <iron-dropdown
+          id="dropdown"
+          with-background
+          no-overlap
+          always-on-top
+          vertical-align="auto"
+          horizontal-align="auto"
+        >
+          <div slot="dropdown-content">
+            <div id="header">${this.header}</div>
+            <!-- Sharingbutton Facebook -->
+            <a
+              href="${this.fburl}"
+              target="_blank"
+              rel="noopener"
+              aria-label="Share on Facebook"
+            >
+              <span id="fbLogo">${fbLogo}</span>
+            </a>
+
+            <!-- Sharingbutton Twitter -->
+            <a
+              href="${this.twurl}"
+              target="_blank"
+              rel="noopener"
+              aria-label="Share on Twitter"
+            >
+              ${twLogo}
+            </a>
+
+            <!-- Sharingbutton E-Mail -->
+            <a
+              href="${this.emailurl}"
+              target="_self"
+              rel="noopener"
+              aria-label="Share by E-Mail"
+            >
+              ${mailLogo}
+            </a>
+
+            <!-- Sharingbutton WhatsApp -->
+            <a
+              href="${this.wappurl}"
+              aria-label="Share on WhatsApp"
+              data-action="share/whatsapp/share"
+              target="_blank"
+            >
+              ${wappLogo}
+            </a>
+          </div>
+        </iron-dropdown>
+      </div>
+    `;
+  }
+
+  firstUpdated() {
+    super.firstUpdated();
+  }
+
+  openDropdown() {
+    this.shadowRoot.getElementById("dropdown").open();
+  }
+}
+
+customElements.define("social-share", socialShare);
